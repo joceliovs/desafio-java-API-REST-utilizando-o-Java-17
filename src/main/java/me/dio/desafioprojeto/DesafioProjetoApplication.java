274eb575663bb6aec8b6e2d3734b0d02cab458f7
joceliovs <joceliovieira@gmail.com>
2023-10-20 09:37:38 -0300

    Subindo o codigo

---
+++ src/main/java/me/dio/desafioprojeto/DesafioProjetoApplication.java
@@ -0,0 +1,13 @@
+package me.dio.desafioprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/desafioprojeto/DesafioProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desafioprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
